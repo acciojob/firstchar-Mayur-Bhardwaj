19576adb7407003e66da485a7ffe86a26e3264fd
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-09-25 15:26:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function firstChar(text) {
   // your code here
-	for(let i=0; i<=text.length; i++){
-		if(text[i] !== " ")
+	for(let i=0; i<text.length; i++){
+		if(text[i] !== " " && text[i] !== "\t" && text[i] !== "\n" && text[i] !== "\r")
 			return text[i];
 	}
 		return "";
-
 }
 
 // Do not change the code below
