904f4bd90eed96a6da7ac4a796e07779c65f18c2
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-10-04 14:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+// function firstChar(text) {
+//   // your code here
+// 	for(let i=0; i<text.length; i++){
+// 		if(text[i] !== " " && text[i] !== "\t" && text[i] !== "\n" && text[i] !== "\r")
+// 			return text[i];
+// 	}
+// 		return "";
+// }
+// let text = "   hello world";
+// alert(firstChar(text));
+
 function firstChar(text) {
-  // your code here
-	for(let i=0; i<text.length; i++){
-		if(text[i] !== " " && text[i] !== "\t" && text[i] !== "\n" && text[i] !== "\r")
-			return text[i];
-	}
-		return "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== " " && text[i] !== "\t" && text[i] !== "\n" && text[i] !== "\r")
+      return text[i];
+  }
+  return "";
 }
+
 let text = "   hello world";
 alert(firstChar(text));
+
