7b45e9286cdfd7601d55a0ae513ae68e3b074b68
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-11-14 16:50:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,14 +9,24 @@
 // let text = "   hello world";
 // alert(firstChar(text));
 
+// function firstChar(text) {
+//   for (let i = 0; i < text.length; i++) {
+//     if (text[i] !== " " && text[i] !== "\t" && text[i] !== "\n" && text[i] !== "\r")
+//       return text[i];
+//   }
+//   return "";
+// }
+
+// let text = "   hello world";
+// alert(firstChar(text));
+
+
 function firstChar(text) {
   for (let i = 0; i < text.length; i++) {
-    if (text[i] !== " " && text[i] !== "\t" && text[i] !== "\n" && text[i] !== "\r")
+    if (text[i] !== " ") {
       return text[i];
+    }
   }
   return "";
 }
-
-let text = "   hello world";
-alert(firstChar(text));
 
